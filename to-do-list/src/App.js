4a70aaa69cc92178f13f873b0a9d39d6fa470054
Eric Larwa <ericlarwa@Erics-MacBook-Air.local>
2024-03-09 16:22:17 -0500

    added footer

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,12 +1,15 @@
 import NavBar from './components/NavBar'
 import TaskInput from './components/TaskInput';
+import Footer from './components/Footer';
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
     <div className='App'>
       <NavBar />
       <TaskInput />
+      <Footer />
     </div>
   )
 };
